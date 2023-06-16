2ac28483232a64ad6d50ce5a2238c0ff21dd7991
Nicholas <NicholasBilbrey18@gmail.com>
2023-06-15 21:54:29 -0400

    Whoops

--- Small_Talk_Frontend/src/app/translate.service.ts
+++ Small_Talk_Frontend/src/app/translate.service.ts
@@ -10,9 +10,7 @@ export class TranslateService {
 
   constructor(private http: HttpClient) { }
 
-  private url: string = 'https://api.cognitive.microsofttranslator.com'
-  private key: string = '8175ed87c33343d884c749f498e164bd'
-  private location: string = 'East US'
+
 
 
 
