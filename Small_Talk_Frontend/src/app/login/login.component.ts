bfc02459ebaee663a2827ebea26835f9bc20fbaf
Nicholas <NicholasBilbrey18@gmail.com>
2023-06-29 19:34:56 -0400

    Login changes

--- Small_Talk_Frontend/src/app/login/login.component.ts
+++ Small_Talk_Frontend/src/app/login/login.component.ts
@@ -10,31 +10,23 @@ import { Translation, User } from '../translation';
 })
 export class LoginComponent {
    constructor(private router: Router, private translateService: TranslateService){}
-  loginUser(userName: string, password: string): void {
-    this.translateService.getUsers().subscribe({
-      next: (users: User[]) => {
-        const user = users.find(u => u.userName === userName && u.password === password);
-        if (user) {
-          this.translateService.currentUser = user; // Assign currentUser value
-          console.log('User logged in:', user);
-          this.router.navigate(['/dictionary']); 
-      }
-      //  else {
-      //      this.translateService.addUser(userName, password).subscribe({
-      //        next: (addUser: Translation) => {
-      //          this.translateService.addUser = addUser;
-      //          console.log('New user created:', addUser);
-      //          this.router.navigate(['/dictionary']); 
-      //        },
-      //        error: (error: any) => {
-      //          console.log('Error creating user:', error);
-      //        }
-      //      });
-      //    }
-        },
+
+   loginUser(userName: string, password: string): void {
+    this.translateService.checkUser(userName, password).subscribe({
+      next: (userId: number) => {
+        if (userId) {
+          this.translateService.currentUser = {
+            userId: userId,
+            userName: userName,
+            password: password
+          };
+          console.log('User Logged in!');
+        }
+      },
       error: (error: any) => {
         console.log('Error retrieving users:', error);
       }
     });
   }
+  
 }
